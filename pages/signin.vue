0f7d5a4a6d4ea7b1568b42d9d82087465a5bf068
Feco <hola@fecocode.com>
2024-07-25 14:35:27 -0300

    Fix(Auth): Signup error

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
+  <div v-if="showView">
     <SignIn />
   </div>
 </template>
 <script lang="ts" setup>
-import { SignIn } from 'vue-clerk';
+import { SignIn, useAuth } from 'vue-clerk';
+import { computed } from 'vue';
+
+const auth = useAuth()
+
+const showView = computed(() => {
+  return auth.isLoaded
+})
 
 // @ts-ignore
 definePageMeta({
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
+  <div v-if="showView">
     <SignUp />
   </div>
 </template>
 <script lang="ts" setup>
-import { SignUp } from 'vue-clerk';
+import { SignUp, useAuth } from 'vue-clerk';
+import { computed } from 'vue';
+
+const auth = useAuth()
+
+const showView = computed(() => {
+  return auth.isLoaded
+})
 
 // @ts-ignore
 definePageMeta({
